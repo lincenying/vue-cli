cb505263a178731ede39ba04b33e2677b06d5a7f
景天 <jingtian@naitang.com>
2016-07-14 14:45:32 +0800

    first update

---
+++ template/src/components/index.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="g-mn">
+        <span>好厉害, 竟然翻到最后一页了...</span>
+    </div>
+</template>
+<script lang="babel">
+    import * as vuexAction from "../store/actions"
+    export default {
+        vuex: {
+            getters: {
+                article: ({article}) => article
+            },
+            actions: vuexAction
+        },
+        components: {
+
+        },
+        computed: {
+
+        },
+        data () {
+            return {
+                page: 1
+            }
+        },
+        methods: {
+
+        },
+        ready() {
+            this.gProgress(100)
+        },
+        route: {
+            canReuse() {
+                return false
+            }
+        }
+    }
+</script>
