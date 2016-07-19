15279b0b2bd634489769ff7818d371c37e1e6b04
景天 <jingtian@naitang.com>
2016-07-19 16:04:19 +0800

    Actions 只处理 mutations 分发

--- template/src/store/actions.js
+++ template/src/store/actions.js
@@ -1,7 +1,6 @@
 /* global window, document */
 
-import Vue from 'vue'
-import ls from 'store2'
+import { ssp } from '../tools/command'
 import * as types from './mutation-types'
 
 export const gLoadding = ({dispatch}, status) => {
@@ -10,17 +9,7 @@ export const gLoadding = ({dispatch}, status) => {
 
 export const gProgress = ({dispatch, state: {route: {path}}}, num) => {
     dispatch(types.GLOBAL_PROGRESS, num)
-    var clientHeight = document.documentElement.clientHeight,
-        scrollTop = ls.get(path)
-    if (num === 100 && scrollTop) {
-        Vue.nextTick(() => {
-            if (document.body.clientHeight >= scrollTop + clientHeight) {
-                window.scrollTo(0, scrollTop)
-            } else {
-                ls.remove(path)
-            }
-        })
-    }
+    ssp(path, num)
 }
 
 export const showMsg = ({dispatch}, content, type = 'error') => {
